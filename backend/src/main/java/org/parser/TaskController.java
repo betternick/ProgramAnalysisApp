8e9176c49d5dec8a0b038aef57f49c0c758e17bf
Tengs Penkwe <tengspenkwe@gmail.com>
2024-03-10 22:26:13 -0700

    move frontend to root direcotory

---
+++ backend/src/main/java/org/parser/TaskController.java
@@ -0,0 +1,26 @@
+package org.servlet;
+
+// import com.example.project.model.Task;
+
+// import com.example.project.service.TaskService;
+// import org.springframework.web.bind.annotation.*;
+
+// @RestController
+// @RequestMapping("/api/tasks")
+public class TaskController {
+
+    // @Autowired
+    // private TaskService taskService;
+
+    // @GetMapping
+    // public List<Task> getAllTasks() {
+    // return taskService.findAll();
+    // }
+
+    // @PostMapping
+    // public Task createTask(@RequestBody Task task) {
+    // return taskService.save(task);
+    // }
+
+    // Additional endpoints (update, delete) can be added here
+}
