fb6dca4fc29eb415fb4e47990c98197736f67130
n7 <mb1313@students.cs.ubc.ca>
2024-03-22 18:04:49 -0700

    implemented and local tested /upload endpoint

--- backend/src/main/java/org/servlet/TaskController.java
+++ backend/src/main/java/org/servlet/TaskController.java
@@ -1,10 +1,17 @@
 package org.servlet;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +24,21 @@ public class TaskController {
         return Arrays.asList(
                 new Task(1, "Task 1", "Description of Task 1 is here"),
                 new Task(2, "Task 2", "Description of Task 2 is there"));
+    }
+
+    private static final String UPLOAD_DIR = "backend/src/main/resources";
+    @PostMapping("/upload")
+    public ResponseEntity<Boolean> uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = StringUtils.cleanPath((file.getOriginalFilename()));
+
+        Path uploadPath = Paths.get(UPLOAD_DIR, fileName);
+
+        try {
+            Files.copy(file.getInputStream(), uploadPath, StandardCopyOption.REPLACE_EXISTING);
+            return ResponseEntity.ok(true);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(false);
+        }
     }
 
     static class Task {
