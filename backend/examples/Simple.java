e2d15a193dabc2207629413f2983ef6f6b1ffdf4
syedwaqarahmed <syedw@student.ubc.ca>
2024-03-24 10:10:10 -0700

    Added labels of "If-else Statement:", "For loop", "While loop:" to the node that has the full code for these statements.

--- backend/examples/Simple.java
+++ backend/examples/Simple.java
@@ -1,26 +1,26 @@
 
 public class Simple {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
-        int g = 123;
-        String h = "fgh";
-
-        for (int i = 0; i < 10; i++) {
-            int myNumber = 10;
-            String what = "whatString";
-            if (g > myNumber) {
-                String gh = "fgghgh";
-                // return; // Adding a return statement here
-            } else {
-                boolean even = isEvenNumber(g);
-            }
-            int waaa = 98;
-        }
-
-        int someInt = 200;
-    }
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//
+//        int g = 123;
+//        String h = "fgh";
+//
+//        for (int i = 0; i < 10; i++) {
+//            int myNumber = 10;
+//            String what = "whatString";
+//            if (g > myNumber) {
+//                String gh = "fgghgh";
+//                // return; // Adding a return statement here
+//            } else {
+//                boolean even = isEvenNumber(g);
+//            }
+//            int waaa = 98;
+//        }
+//
+//        int someInt = 200;
+//    }
 
     // public static void main(String[] args) {
     // System.out.println("Hello, World!");
@@ -80,27 +80,27 @@ public class Simple {
     // return 6;
     // }
 
-    public static int sampleForLoopInsideIF(int num1, int num2) {
-        // nestedFunction();
-        int g = 123;
-        String h = "fgh";
-        if (h != "f") {
-            for (int g = 0; g < 10; g++) {
-                int myNumber = 10;
-                String what = "whatString";
-            }
-            int afterLoopInt = 5000;
-        } else {
-            int someInt = 200;
-            return 25;
-        }
-
-        if (h == "WHATTT") {
-            int someINTman = 566;
-        }
-        int endInt = 5;
-        return 6;
-    }
+//    public static int sampleForLoopInsideIF(int num1, int num2) {
+//        // nestedFunction();
+//        int g = 123;
+//        String h = "fgh";
+//        if (h != "f") {
+//            for (int g = 0; g < 10; g++) {
+//                int myNumber = 10;
+//                String what = "whatString";
+//            }
+//            int afterLoopInt = 5000;
+//        } else {
+//            int someInt = 200;
+//            return 25;
+//        }
+//
+//        if (h == "WHATTT") {
+//            int someINTman = 566;
+//        }
+//        int endInt = 5;
+//        return 6;
+//    }
     //
     // public static void nestedFunction() {
     // System.out.println("This is a nested function");
@@ -110,4 +110,32 @@ public class Simple {
     // public static boolean isEvenNumber(int num) {
     // return num % 2 == 0;
     // }
+
+    int sampleIfProgram(int a) {
+        int b = a;
+        if (b > 10) {
+            b++;
+        } else {
+            b--;
+            return;
+        }
+        System.out.println("Yay");
+    }
+
+    int sampleForLoopProgram(int a) {
+        int b = a;
+        for (int count = 0; count < 10; count++) {
+            b++;
+        }
+        System.out.println("Yay");
+    }
+
+    int sampleWhileLoopProgram(int a) {
+        int b = a;
+        while (b > 10) {
+            b--;
+        }
+        System.out.println("Yay");
+    }
+
 }
