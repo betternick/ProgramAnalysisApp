7115e97a30aec1cb1d28e0b87a7670dcde64d42a
Tengs Penkwe <tengspenkwe@gmail.com>
2024-03-27 19:08:45 -0700

    Introduced the Singleton pattern to the Executor class, ensuring efficient resource management. Added functionality for compiling Java files into JARs and executing them with a Java agent, streamlining the process. This update simplifies execution flows, enhances reliability, and sets the stage for future enhancements in dynamic class loading and execution scenarios.

--- backend/src/main/java/org/profile/Executor.java
+++ backend/src/main/java/org/profile/Executor.java
@@ -4,74 +4,133 @@ import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.JavaFileObject;
-import java.io.File;
+import java.io.*;
+import java.util.jar.*;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 public class Executor {
-    public void executeJavaFileWithAgent(String filePath, String agentJarPath, String jarToRun, String fullClassName)
+    private static Executor instance = null;
+
+    private Executor() {
+    }
+
+    public static synchronized Executor getInstance() {
+        if (instance == null) {
+            instance = new Executor();
+        }
+        return instance;
+    }
+
+    private void createJar(String jarFileName, String classFilePath, String entryPoint) throws IOException {
+        Manifest manifest = new Manifest();
+        manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
+        manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS, entryPoint);
+
+        JarOutputStream target = new JarOutputStream(new FileOutputStream(jarFileName), manifest);
+        File classFile = new File(classFilePath);
+        addToJar(classFile, target);
+        target.close();
+    }
+
+    private void addToJar(File source, JarOutputStream target) throws IOException {
+        BufferedInputStream in = null;
+        try {
+            if (source.isDirectory()) {
+                String name = source.getPath().replace("\\", "/");
+                if (!name.isEmpty()) {
+                    if (!name.endsWith("/"))
+                        name += "/";
+                    JarEntry entry = new JarEntry(name);
+                    entry.setTime(source.lastModified());
+                    target.putNextEntry(entry);
+                    target.closeEntry();
+                }
+                for (File nestedFile : source.listFiles())
+                    addToJar(nestedFile, target);
+                return;
+            }
+
+            JarEntry entry = new JarEntry(source.getPath().replace("\\", "/"));
+            entry.setTime(source.lastModified());
+            target.putNextEntry(entry);
+            in = new BufferedInputStream(new FileInputStream(source));
+
+            byte[] buffer = new byte[1024];
+            while (true) {
+                int count = in.read(buffer);
+                if (count == -1)
+                    break;
+                target.write(buffer, 0, count);
+            }
+            target.closeEntry();
+        } finally {
+            if (in != null)
+                in.close();
+        }
+    }
+
+    public void execute(String filePath, String fullClassName, String graphPath) {
+        String agentPath = ProfilingAgent.getAgentPath();
+
+        try {
+            // Step 1: Compile the Java file
+            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+            StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+
+            File sourceFile = new File(filePath);
+            Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjects(sourceFile);
+            boolean compilationResult = compiler.getTask(null, fileManager, null, null, null, compilationUnits).call();
+
+            if (!compilationResult) {
+                System.out.println("Compilation Failed");
+                return;
+            }
+            System.out.println("Compilation is successful");
+            fileManager.close();
+
+            // Step 2: Create a JAR file from the compiled classes
+            // Assuming the compiled class file name matches the source file name but with
+            // .class extension
+            String classFilePath = filePath.replace(".java", ".class");
+            String jarFileName = filePath.replace(".java", ".jar");
+            createJar(jarFileName, classFilePath, fullClassName);
+
+            // Step 3: Execute the JAR file with the Java agent
+            executeJavaFileWithAgent(filePath, agentPath, jarFileName, graphPath, fullClassName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // This is only for testing purposes
+    public static void main(String[] args) throws Exception {
+        System.out.println("Executor Initialized");
+        // new Executor().executeJavaFileWithAgent("examples/Simple.java", "build/libs/backend-agent.jar",
+        //         "examples/Simple.jar", "Simple");
+    }
+
+    private void executeJavaFileWithAgent(String filePath, String agentJarPath, String jarToRun, String graphPath, String fullClassName)
             throws Exception {
 
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
-
-        File sourceFile = new File(filePath);
-        Iterable<? extends JavaFileObject> compilationUnits = fileManager
-                .getJavaFileObjectsFromFiles(java.util.Collections.singletonList(sourceFile));
-        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, null, null, null, compilationUnits);
-
-        if (task.call()) {
-            System.out.println("Compilation is successful");
-        } else {
-            System.out.println("Compilation Failed");
-            return;
-        }
-
-        // // 2. Construct classpath including the directory of the compiled classes.
-        // Path compiledClassesDir = Paths.get(sourceFile.getParent(), "bin");
-        // Files.createDirectories(compiledClassesDir);
-        // URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] {
-        // compiledClassesDir.toUri().toURL() });
-
-        // // 3. Extract the class name and package from the file path.
-        // String className = extractClassNameFromFilePath(sourceFilePath);
-
-        // // 4. Construct the command to run the Java application with the agent.
-        // ProcessBuilder builder = new ProcessBuilder(
-        // "java", "-javaagent:" + agentJarPath, "-cp", compiledClassesDir.toString(),
-        // className);
-        // builder.inheritIO();
-
-        // // 5. Start the process and capture the output.
-        // Process process = builder.start();
-        // process.waitFor();
         System.out.println("Running the Java application with the agent...");
-
-        // ProcessBuilder builder = new ProcessBuilder(
-        // "java",
-        // "-javaagent:build/libs/backend-agent.jar", // Your agent
-        // "-javaagent:byte-buddy-agent-1.14.12.jar", // Byte Buddy agent
-        // "-cp", "examples/Simple.jar:byte-buddy-1.14.12.jar", // Classpath
-        // "Simple" // The main class name of your application
-        // );
 
         ProcessBuilder builder = new ProcessBuilder(
                 "java",
-                // "-javaagent:" + agentJarPath,
-                "-javaagent:" + "build/libs/backend-agent.jar",
-                // "-javaagent:" + "byte-buddy-agent-1.14.12.jar",
-                "-cp", "examples/Simple.jar" + ":byte-buddy-1.14.12.jar"
-                        + ":asm-9.6.jar" + ":asm-commons-9.6.jar",
-                // fullClassName,
-                "Simple");
+                "-javaagent:" + agentJarPath + "=" + graphPath,
+                "-cp",  jarToRun 
+                        + ":byte-buddy-1.14.12.jar"
+                        + ":asm-9.6.jar" + ":asm-commons-9.6.jar"
+                        + ":log4j-api-2.23.1.jar" + ":log4j-core-2.23.1.jar" + ":log4j-1.2-api-2.23.1.jar",
+                fullClassName);
         builder.inheritIO();
         Process process = builder.start();
         process.waitFor();
         System.out.println("Java application with the agent finished.");
     }
 
-    public void executeJavaFile(String filePath) throws Exception {
+    private void executeJavaFile(String filePath) throws Exception {
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         int compilationResult = compiler.run(null, null, null, filePath);
         if (compilationResult == 0) {
@@ -105,11 +164,5 @@ public class Executor {
         // }
         // return filePath;
         return "Simple";
-    }
-
-    public static void main(String[] args) throws Exception {
-        // new Executor().executeJavaFile("examples/Simple.java");
-        new Executor().executeJavaFileWithAgent("examples/Simple.java", "build/libs/backend-agent.jar",
-                "examples/Simple.jar", "Simple");
     }
 }
